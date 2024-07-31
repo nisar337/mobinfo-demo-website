f90ced41d86e5606cee3ccc110ec0d00f4c843f8
M Nisar <114214962+nisar337@users.noreply.github.com>
2024-07-31 10:29:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+
+// active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop  > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+// nav hide 
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+ 
+// counter design
+ document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration /  range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1", 61500, 62000, 3000);
+    counter("count2", 100, 350, 3000);
+    counter("count3", 600, 999, 3000);
+    counter("count4", 99950, 100000, 3000);
+ });
+   
+   
+
